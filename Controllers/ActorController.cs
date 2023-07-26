bce61c63bff75c5539b83bfd7ac99e17561dbc68
Adam Elias <122506412+elias-adam-gh@users.noreply.github.com>
2023-07-26 19:40:15 -0400

    started configuring controllers

---
+++ Controllers/ActorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using theSite.Data;
+
+namespace theSite.Controllers
+{
+    public class ActorController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
